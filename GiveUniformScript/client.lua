cd4c5a4920b740ed7468be7a61ddf6cafd25bca6
KA1N3D2024 <kaine.duncan@ou.ac.uk>
2024-09-27 01:22:21 +0100

    Add files via upload

--- GiveUniformScript/client.lua
+++ GiveUniformScript/client.lua
@@ -1,47 +1,104 @@
--- client.lua
+-- KA1N3D Software - Standalone Uniform Sharing Script
 
-local receivedUniforms = {}
+local OutstandingRequest = nil
 
--- Handle the accept uniform prompt
-RegisterNetEvent('ka1n3d:promptAcceptUniform')
-AddEventHandler('ka1n3d:promptAcceptUniform', function(sourcePlayerId, eupComponents)
-    receivedUniforms[sourcePlayerId] = eupComponents
-    TriggerEvent('chat:addMessage', {
-        color = {255, 255, 0},
-        multiline = true,
-        args = {"System", "Player ID: " .. sourcePlayerId .. " wants to give you their uniform. Type /acceptuniform " .. sourcePlayerId .. " to accept."}
-    })
+-- Command to Give Uniform
+RegisterCommand("giveUniform", function(source, args, rawCommand)
+    if #args < 1 then
+        TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'You must provide a player ID to send your uniform to.')
+        print("[GiveUniform] No player was specified.")
+        return
+    end
+
+    if tonumber(args[1]) == nil then
+        TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'You must provide a valid player ID.')
+        print("[GiveUniform] Invalid player was specified.")
+        return
+    end
+
+    local targetPlayerId = tonumber(args[1])
+    local playerCheck = Player(targetPlayerId).state.displayName
+
+    if playerCheck == nil then
+        TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'The player you tried to give your uniform to does not exist.')
+        print("[GiveUniform] Invalid player was specified.")
+        return
+    end
+
+    local SentData = {
+        Clothing = {},
+        Props = {}
+    }
+
+    local ownPed = PlayerPedId()
+
+    -- Gather Clothing Data
+    for index = 0, 11 do
+        if index ~= 0 and index ~= 2 then
+            local drawable = GetPedDrawableVariation(ownPed, index)
+            local texture = GetPedTextureVariation(ownPed, index)
+            table.insert(SentData.Clothing, {
+                component = index,
+                drawable = drawable,
+                texture = texture
+            })
+        end
+    end
+
+    -- Gather Prop Data (hats, glasses, etc.)
+    for index = 0, 2 do
+        local drawable = GetPedPropIndex(ownPed, index)
+        local texture = GetPedPropTextureIndex(ownPed, index)
+        table.insert(SentData.Props, {
+            component = index,
+            drawable = drawable,
+            texture = texture
+        })
+    end
+
+    -- Notify the sender
+    TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'You have sent ' .. playerCheck .. ' your uniform.')
+
+    -- Send uniform to server
+    TriggerServerEvent("ka1n3dSoftware:sendUniform", targetPlayerId, SentData)
 end)
 
--- Function to apply EUP components to a player
-function applyEUPToPlayer(player, eupComponents)
-    for componentId, drawable in pairs(eupComponents) do
-        SetPedComponentVariation(player, componentId, drawable, 0, 0)
-    end
-end
+-- Event triggered when the uniform is sent
+RegisterNetEvent("ka1n3dSoftware:receiveUniform")
+AddEventHandler("ka1n3dSoftware:receiveUniform", function(fromPlayer, clothingData)
+    OutstandingRequest = clothingData
 
--- Registering the /acceptuniform command
-RegisterCommand('acceptuniform', function(source, args)
-    local requestingPlayerId = tonumber(args[1])
-    if requestingPlayerId and receivedUniforms[requestingPlayerId] then
-        local eupComponents = receivedUniforms[requestingPlayerId]
-        applyEUPToPlayer(PlayerPedId(), eupComponents)
-
-        -- Notify both players that the uniform was successfully applied
-        TriggerServerEvent('ka1n3d:notifyUniformAccepted', requestingPlayerId)
-        TriggerEvent('chat:addMessage', {
-            color = {0, 255, 0},
-            multiline = true,
-            args = {"System", "You have accepted the uniform from Player ID: " .. requestingPlayerId}
-        })
-
-        receivedUniforms[requestingPlayerId] = nil
-    else
-        TriggerEvent('chat:addMessage', {
-            color = {255, 0, 0},
-            multiline = true,
-            args = {"System", "No uniform request found from Player ID: " .. (requestingPlayerId or "unknown")}
-        })
-    end
+    -- Notify the receiving player
+    TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, Player(tonumber(fromPlayer)).state.displayName .. ' has sent you their uniform. Type /acceptUniform to accept or /denyUniform to reject.')
 end)
 
+-- Command to Deny Uniform
+RegisterCommand("denyUniform", function(source, args, rawCommand)
+    OutstandingRequest = nil
+
+    -- Notify the player
+    TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'You have rejected the uniform request.')
+end)
+
+-- Command to Accept Uniform
+RegisterCommand("acceptUniform", function(source, args, rawCommand)
+    if OutstandingRequest == nil then
+        TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'There is no uniform request to accept.')
+        return
+    end
+
+    local ownPed = PlayerPedId()
+
+    -- Apply the received uniform
+    for _, data in pairs(OutstandingRequest.Clothing) do
+        SetPedComponentVariation(ownPed, data.component, data.drawable, data.texture, 0)
+    end
+
+    for _, data in pairs(OutstandingRequest.Props) do
+        SetPedPropIndex(ownPed, data.component, data.drawable, data.texture, 0)
+    end
+
+    -- Clear the request and notify the player
+    OutstandingRequest = nil
+    TriggerEvent('chatMessage', "Give Uniform", {226, 0, 57}, 'You have accepted the uniform request.')
+end)
--- GiveUniformScript/fxmanifest.lua
+++ GiveUniformScript/fxmanifest.lua
@@ -1,9 +1,8 @@
--- fxmanifest.lua
 fx_version 'cerulean'
 game 'gta5'
 
 author 'KA1N3D Software FiveM'
-description 'A simple EUP uniform exchange script for FiveM'
+description 'Standalone Uniform Sharing System for FiveM'
 version '1.0.0'
 
 client_script 'client.lua'
